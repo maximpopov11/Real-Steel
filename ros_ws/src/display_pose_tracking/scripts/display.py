876abe5c131b4ff89816ffbd96e9d72192c94afd
Zach <zhc@iastate.edu>
2025-03-03 00:25:29 -0600

    add hips, nose, and hands, refactor/rename

--- ros_ws/src/display_pose_tracking/scripts/display.py
+++ ros_ws/src/display_pose_tracking/scripts/display.py
@@ -1,5 +1,5 @@
 import rospy
-from custom_msg.msg import arm
+from custom_msg.msg import landmarks
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
@@ -16,78 +16,88 @@ ax.view_init(elev=90, azim=90)  # Top-down view looking at the XY plane
 instant_received = 0
 x_coords, y_coords, z_coords = [], [], []
 def plot_landmarks_in_3d(msg):
-    #print("msg received!", msg)
-    #return
-    #landmarks = landmark_frame[0]
-    # print(landmarks)
-
-    #ax.clear()  # Clear previous points
-
-    # Extract the 3D coordinates (x, y, z)
-    #x_coords = [landmark.x for landmark in landmarks]  # Left/Right
-    #y_coords = [landmark.y for landmark in landmarks]  # Up/Down
-    #z_coords = [landmark.z for landmark in landmarks]  # Forward/Backward
     global x_coords, y_coords, z_coords, instant_received
+    # Set the moment we got these for the while loop later to use
     instant_received = time.time()
+    # Extract x, y, z coords into arrays for matplotlib to graph
     x_coords = [
+        msg.left_hip[0],
+        msg.right_hip[0],
         msg.left_shoulder[0],
-        msg.left_elbow[0],
-        msg.left_wrist[0],
         msg.right_shoulder[0],
+        msg.left_elbow[0],
         msg.right_elbow[0],
-        msg.right_wrist[0]
+        msg.left_wrist[0],
+        msg.right_wrist[0],
+        msg.left_pinky[0],
+        msg.right_pinky[0],
+        msg.left_index[0],
+        msg.right_index[0],
+        msg.left_thumb[0],
+        msg.right_thumb[0],
+        msg.nose[0]
     ]
     y_coords = [
+        msg.left_hip[1],
+        msg.right_hip[1],
         msg.left_shoulder[1],
-        msg.left_elbow[1],
-        msg.left_wrist[1],
         msg.right_shoulder[1],
+        msg.left_elbow[1],
         msg.right_elbow[1],
-        msg.right_wrist[1]
+        msg.left_wrist[1],
+        msg.right_wrist[1],
+        msg.left_pinky[1],
+        msg.right_pinky[1],
+        msg.left_index[1],
+        msg.right_index[1],
+        msg.left_thumb[1],
+        msg.right_thumb[1],
+        msg.nose[1]
     ]
     z_coords = [
+        msg.left_hip[2],
+        msg.right_hip[2],
         msg.left_shoulder[2],
-        msg.left_elbow[2],
-        msg.left_wrist[2],
         msg.right_shoulder[2],
+        msg.left_elbow[2],
         msg.right_elbow[2],
-        msg.right_wrist[2]
+        msg.left_wrist[2],
+        msg.right_wrist[2],
+        msg.left_pinky[2],
+        msg.right_pinky[2],
+        msg.left_index[2],
+        msg.right_index[2],
+        msg.left_thumb[2],
+        msg.right_thumb[2],
+        msg.nose[2]
     ]
 
-    #ax.scatter(x_coords, y_coords, z_coords)
-    #plt.draw()
-    #plt.pause(0.1)  # Small pause to allow the figure to update
-
-#    print(f"Found {len(landmark_frames) - skipped} / {len(landmark_frames)}")
-
 def app():
-    sub = rospy.Subscriber('arms', arm, plot_landmarks_in_3d)
+    sub = rospy.Subscriber('landmarks', landmarks, plot_landmarks_in_3d)
     rospy.init_node('POSE_Display', anonymous=True)
 
     while True:
         #print("xcoords", x_coords)
+        # Ensure the bounds of the graph are drawn correctly
         ax.set_xlim([0, 640])
         ax.set_ylim([0, 480])
-        ax.set_zlim([0, 4])
+        ax.set_zlim([4, 0])
         ax.set_xlabel("X (Left/Right)")
         ax.set_ylabel("Y (Up/Down)")
         ax.set_zlabel("Z (Forward/Backward)")
 
         plt.draw()
-        plt.pause(0.1)  # Small pause to allow the figure to update
 
+        # Small pause to allow the figure to update
+        plt.pause(0.001)  
+
+        # If the last frame was more than a tenth of a second ago, skip redrawing it
         if time.time() - instant_received > .1:
             continue
 
         ax.clear()  # Clear previous points
-        ax.scatter(x_coords, y_coords, z_coords, color=['red', 'orange', 'black', 'green', 'blue', 'purple'])
-        print(f"Left Wrist: ({x_coords[2]}, {y_coords[2]}, {z_coords[2]})")
-
-#        plt.draw()
- #       plt.pause(0.1)  # Small pause to allow the figure to update
-
-    rospy.spin()
-    
+        ax.scatter(x_coords, y_coords, z_coords, color=['red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'black', 'black', 'black', 'black', 'green', 'green', 'purple'])
+        print(f"Left Wrist: ({x_coords[2]}, {y_coords[2]}, {z_coords[2]})")    
     
 if __name__ == '__main__':
     try:
