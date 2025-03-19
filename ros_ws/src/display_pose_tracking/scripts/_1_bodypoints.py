ff2f160211c7ca7b17e09c171b53f9a14f41143c
Zach <zhc@iastate.edu>
2025-03-19 11:49:47 -0500

    flesh out publish_frames and get_bodypoints

--- ros_ws/src/display_pose_tracking/scripts/_1_bodypoints.py
+++ ros_ws/src/display_pose_tracking/scripts/_1_bodypoints.py
@@ -5,3 +5,46 @@ def get_bodypoints(frame_pair: Frame_Pair_t, bodypoints_queue: q_Bodypoints_t):
     Get raw bodypoints using Mediapipe.
     """
     pass
+
+"""
+from typing import Dict
+import pyrealsense2 as rs
+import numpy as np
+from time import time
+from model import Bodypoints
+from util import MP, setup_depth_pipeline, make_detect_async_callback, detect_with_callback, MP_FULL_MODEL_PATH
+from custom_types import *
+
+def publish_frame(rospy, frame_queue: Dict[int, Frame_Pair]):
+    Using a camera, publish video frames to the
+    given indexed queue at regular intervals.
+
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+    pipeline = setup_depth_pipeline()
+    while not rospy.is_shutdown():
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        aligned_frames = align.process(frames)
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+
+        color_frame = frames.get_color_frame()
+        if not aligned_depth_frame or not color_frame:
+            continue
+
+        # Convert color frame to numpy array
+        color_frame_array = np.asanyarray(color_frame.get_data())
+
+        frame_queue[int(time() * 1000)] = (color_frame_array, depth_frame)
+
+
+def get_bodypoints(frame_pair : Frame_Pair, bodypoints_queue: Dict[int, Bodypoints]) -> Bodypoints:
+    
+    Get raw bodypoints using Mediapipe.
+    mp_image = MP.Image(image_format=MP.ImageFormat.SRGB, data=frame_pair[0])
+    model_path = MP_FULL_MODEL_PATH
+    callback = make_detect_async_callback(frame_pair[1], bodypoints_queue)
+    
+    detect_with_callback(mp_image, model_path, callback)
+
+"""
--- ros_ws/src/display_pose_tracking/scripts/util.py
+++ ros_ws/src/display_pose_tracking/scripts/util.py
@@ -1,8 +1,35 @@
 import pyrealsense2 as rs
 from time import time
+import mediapipe as MP
+from time import time
+from _collections_abc import Callable
+import numpy.typing as nptyping
+
+mp_pose = MP.solutions.pose
+PoseLandmarkerResult = MP.tasks.vision.PoseLandmarkerResult
+BaseOptions = MP.tasks.BaseOptions
+PoseLandmarker = MP.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = MP.tasks.vision.PoseLandmarkerOptions
+VisionRunningMode = MP.tasks.vision.RunningMode
+mpImage = MP.Image
+
+Result_Depth_Callback = Callable[[PoseLandmarkerResult, mpImage, int], None]
+"""
+Type of the callback that receives the result of mediapipe detect_async.
+"""
 
 CAMERA_IMAGE_WIDTH = 640
 CAMERA_IMAGE_HEIGHT = 480
+
+LIB_DIR_PATH = "/home/zhc/Documents/ISU/cs402/sd15_reel-steel/ros_ws/src/display_pose_tracking/lib"
+MP_LITE_MODEL_PATH = f"{LIB_DIR_PATH}/pose_landmarker_lite.task"
+MP_FULL_MODEL_PATH = f"{LIB_DIR_PATH}/pose_landmarker_full.task"
+MP_HEAVY_MODEL_PATH = f"{LIB_DIR_PATH}/pose_landmarker_heavy.task"
+
+LIB_DIR_PATH = "/home/zhc/Documents/ISU/cs402/sd15_reel-steel/ros_ws/src/display_pose_tracking/lib"
+MP_LITE_MODEL_PATH = f"{LIB_DIR_PATH}/pose_landmarker_lite.task"
+MP_FULL_MODEL_PATH = f"{LIB_DIR_PATH}/pose_landmarker_full.task"
+MP_HEAVY_MODEL_PATH = f"{LIB_DIR_PATH}/pose_landmarker_heavy.task"
 
 def timestamp():
     """Returns the current time in milliseconds since the epoch using time()."""
@@ -17,7 +44,7 @@ def setup_depth_pipeline():
     pipeline_wrapper = rs.pipeline_wrapper(pipeline)
     pipeline_profile = config.resolve(pipeline_wrapper)
     device = pipeline_profile.get_device()
-    device_product_line = str(device.get_info(rs.camera_info.product_line))
+    #device_product_line = str(device.get_info(rs.camera_info.product_line))
 
     found_rgb = False
     for s in device.sensors:
@@ -35,19 +62,19 @@ def setup_depth_pipeline():
     pipeline.start(config)
     return pipeline
 
-def make_detect_async_callback(depth_frame):
-    def callback(result: PoseLandmarkerResult, output_image : mp.Image, timestamp_ms: int):
+def make_detect_async_callback(depth_frame, bodypoints_queue):
+    def callback(result: PoseLandmarkerResult, output_image : MP.Image, timestamp_ms: int):
         if len(result.pose_landmarks) == 0:
             return
 
-        relevant_landmarks = get_relevant_landmarks(result)
-        print(f"Arm: {relevant_landmarks}")
+        landmarks = result[0]
+        print(f"Landmarks: {landmarks}")
 
         landmarks_with_depth = []
-        for point in relevant_landmarks:
-            x_coord = round(point.x * 640)
-            y_coord = round(point.y * 480)
-            if x_coord >= CAMERA_WIDTH or x_coord < 0 or y_coord < 0 or y_coord >= CAMERA_HEIGHT:
+        for point in landmarks:
+            x_coord = round(point.x * CAMERA_IMAGE_WIDTH)
+            y_coord = round(point.y * CAMERA_IMAGE_WIDTH)
+            if x_coord >= CAMERA_IMAGE_WIDTH or x_coord < 0 or y_coord < 0 or y_coord >= CAMERA_IMAGE_HEIGHT:
                 print(f"OUT OF BOUNDS ({x_coord}, {y_coord})")
                 landmarks_with_depth.append((x_coord, y_coord, 0))
                 return
@@ -56,26 +83,14 @@ def make_detect_async_callback(depth_frame):
             landmarks_with_depth.append((x_coord, y_coord, z_coord))
             print(f"READ ({int(time()*1000) - timestamp_ms}ms): {landmarks_with_depth}")
 
-        msg = landmarks()
-        msg.left_hip = landmarks_with_depth[0]
-        msg.right_hip = landmarks_with_depth[1]
-        msg.left_shoulder = landmarks_with_depth[2]
-        msg.right_shoulder = landmarks_with_depth[3]
-        msg.left_elbow = landmarks_with_depth[4]
-        msg.right_elbow = landmarks_with_depth[5]
-        msg.left_wrist = landmarks_with_depth[6]
-        msg.right_wrist = landmarks_with_depth[7]
-        msg.left_pinky = landmarks_with_depth[8]
-        msg.right_pinky = landmarks_with_depth[9]
-        msg.left_index = landmarks_with_depth[10]
-        msg.right_index = landmarks_with_depth[11]
-        msg.left_thumb = landmarks_with_depth[12]
-        msg.right_thumb = landmarks_with_depth[13]
-        msg.nose = landmarks_with_depth[14]
-
-
-        # rospy.loginfo(msg) # will continue to log in CLI msg being sent
-        print("sent!")
-        pub.publish(msg)
+        bodypoints_queue[timestamp_ms] = landmarks_with_depth
 
     return callback
+
+def detect_with_callback(mp_image : nptyping.NDArray, model_path : str, callback : Result_Depth_Callback):
+    with PoseLandmarker.create_from_options(PoseLandmarkerOptions(
+        base_options=BaseOptions(model_asset_path=model_path),
+        running_mode=VisionRunningMode.LIVE_STREAM,
+        result_callback=callback
+    )) as landmarker:
+        landmarker.detect_async(mp_image, int(time() * 1000))
