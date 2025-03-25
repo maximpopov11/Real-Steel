4e897a68dd35f4a9be23c2bf6defe7061d180a90
maximpopov11 <icecreamguy0@gmail.com>
2025-03-25 09:33:29 -0500

    Update signatures more

--- ros_ws/src/display_pose_tracking/scripts/_2_processing.py
+++ ros_ws/src/display_pose_tracking/scripts/_2_processing.py
@@ -1,29 +1,24 @@
-from typing import Dict, List
-from custom_types import ts_Bodypoints_t, q_Robot_Angles_t, Bodypoints_t, Robot_Angles_t
-from asyncio import sleep
-from heapq import heappush, heappop
+from custom_types import Bodypoints_t, Robot_Angles_t
+from queue import PriorityQueue
+from typing import Dict
 
-async def generate_mock_bodypoints(bodypoints_by_timestamp: ts_Bodypoints_t, robot_angles_queue: q_Robot_Angles_t,):
-    await sleep(.02)
 
-    heappush(robot_angles_queue, (bodypoints_by_timestamp[0], [0, 0, 0, 0]))
-
-# TODO: plug into framework file
-# TODO: we need to add locks to all of our multi-threaded heaps
+# TODO: add placeholders for all funcs to be able to run through this all and output dummy data for what's not done
+# TODO: plug into framework file, signatures changed
 def process_bodypoints(
-    timestamps: List[int],
+    timestamps: PriorityQueue[int],
     bodypoints_by_timestamp: Dict[int, Bodypoints_t],
-    robot_angles_queue: q_Robot_Angles_t,
+    robot_angles_by_timestamp: Dict[int, Robot_Angles_t],
 ):
     """
     Spawn threads to process Bodypoints and load them into the RobotAngles queue.
     """
     # TODO: this is a placeholder for whatever robot bodypoints will actually be
-    robot_bodypoints_by_timestamp: Dict[int, ts_Bodypoints_t] = {}
+    robot_bodypoints_by_timestamp: Dict[int, Bodypoints_t] = {}
 
     # TODO: thread max_parallelism at a time, wait if all done until more come in
     # TODO: are we ever looking to the future for things we might have already calculated? If so, do they ever wait and look at the past? How much complexity right now?
-    timestamp = heappop(timestamps)
+    timestamp = timestamps.get()
     
     find_missing_points(bodypoints_by_timestamp, timestamp)
     smooth_points(bodypoints_by_timestamp, timestamp)
@@ -39,7 +34,7 @@ def process_bodypoints(
     robot_bodypoints_by_timestamp[timestamp] = final_bodypoints
 
     final_angles = get_robotangles_from_robot_bodypoints(final_bodypoints)
-    heappush(robot_angles_queue, (timestamp, final_angles))
+    robot_angles_by_timestamp[timestamp] = final_angles
 
 
 # TODO: these aren't all working in a consistent manner, let's be consistent
@@ -87,7 +82,7 @@ def restrain_position(robot_angles: Robot_Angles_t) -> Bodypoints_t:
     pass
 
 
-def restrain_speed(robot_bodypoints_by_timestamp: dict[int, ts_Bodypoints_t], timestamp: int) -> Bodypoints_t:
+def restrain_speed(robot_bodypoints_by_timestamp: dict[int, Bodypoints_t], timestamp: int) -> Bodypoints_t:
     """
     Restrain the robot bodypoints at the given timestamp to disallow moving faster than the threshold.
     """
