ec71521e3cb4d1a0f49ba39cb8ac4fcfe717acb0
maximpopov11 <icecreamguy0@gmail.com>
2025-03-26 08:58:16 -0500

    Private internals

--- ros_ws/src/display_pose_tracking/scripts/_2_processing.py
+++ ros_ws/src/display_pose_tracking/scripts/_2_processing.py
@@ -30,28 +30,27 @@ def _process_bodypoints_loop(
     """
     Main processing loop that runs in a separate thread.
     """
-    # TODO: this is a placeholder for whatever robot bodypoints will actually be
     robot_bodypoints_by_timestamp: Dict[int, Bodypoints_t] = {}
 
     while True:
         timestamp = timestamps.get()  # Blocks efficiently until data is available
         
-        find_missing_points(bodypoints_by_timestamp, timestamp)
-        smooth_points(bodypoints_by_timestamp, timestamp)
+        _find_missing_points(bodypoints_by_timestamp, timestamp)
+        _smooth_points(bodypoints_by_timestamp, timestamp)
         bodypoints = bodypoints_by_timestamp[timestamp]
 
-        robot_angles = get_robotangles(bodypoints)
-        restrained_angles = restrain_angles(robot_angles)
+        robot_angles = _get_robotangles(bodypoints)
+        restrained_angles = _restrain_angles(robot_angles)
 
-        restrain_position(restrained_angles, robot_bodypoints_by_timestamp, timestamp)
-        restrain_speed(robot_bodypoints_by_timestamp, timestamp)
+        _restrain_position(restrained_angles, robot_bodypoints_by_timestamp, timestamp)
+        _restrain_speed(robot_bodypoints_by_timestamp, timestamp)
         
         robot_bodypoints = robot_bodypoints_by_timestamp[timestamp]
-        final_angles = get_robotangles_from_robot_bodypoints(robot_bodypoints)
+        final_angles = _get_robotangles_from_robot_bodypoints(robot_bodypoints)
         robot_angles_by_timestamp[timestamp] = final_angles
 
 
-def find_missing_points(bodypoints_by_timestamp: Dict[int, Bodypoints_t], timestamp: int):
+def _find_missing_points(bodypoints_by_timestamp: Dict[int, Bodypoints_t], timestamp: int):
     """
     Use points in surrounding frames to populate guesses for any unfound bodypoints at the timestamp.
     """
@@ -59,7 +58,7 @@ def find_missing_points(bodypoints_by_timestamp: Dict[int, Bodypoints_t], timest
     pass
 
 
-def smooth_points(bodypoints_by_timestamp: Dict[int, Bodypoints_t], timestamp: int):
+def _smooth_points(bodypoints_by_timestamp: Dict[int, Bodypoints_t], timestamp: int):
     """
     Use points in surrounding frames to smoothen points at the timestamp, ignoring variations within a small margin.
     """
@@ -67,28 +66,28 @@ def smooth_points(bodypoints_by_timestamp: Dict[int, Bodypoints_t], timestamp: i
     pass
 
 
-def get_robotangles(bodypoints: Bodypoints_t) -> Robot_Angles_t:
+def _get_robotangles(bodypoints: Bodypoints_t) -> Robot_Angles_t:
     """
     Map the given bodypoints to robot angles.
     """
     pass
 
 
-def get_robotangles_from_robot_bodypoints(bodypoints: Bodypoints_t) -> Robot_Angles_t:
+def _get_robotangles_from_robot_bodypoints(bodypoints: Bodypoints_t) -> Robot_Angles_t:
     """
     Map the given robot bodypoints to robot angles.
     """
     pass
 
 
-def restrain_angles(robot_angles: Robot_Angles_t) -> Robot_Angles_t:
+def _restrain_angles(robot_angles: Robot_Angles_t) -> Robot_Angles_t:
     """
     Restrain the given robot_angles to disallow any infeasible angles.
     """
     pass
 
 
-def restrain_position(
+def _restrain_position(
     robot_angles: Robot_Angles_t, 
     robot_bodypoints_by_timestamp: Dict[int, Bodypoints_t],
     timestamp: int
@@ -101,7 +100,7 @@ def restrain_position(
     pass
 
 
-def restrain_speed(robot_bodypoints_by_timestamp: Dict[int, Bodypoints_t], timestamp: int):
+def _restrain_speed(robot_bodypoints_by_timestamp: Dict[int, Bodypoints_t], timestamp: int):
     """
     Restrain the robot bodypoints at the given timestamp to disallow moving faster than the threshold.
     """
