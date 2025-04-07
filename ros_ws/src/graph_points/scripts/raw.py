cb74b0a9013ab812f88f0caac0150b569eb4671d
Isaac Lo <isaaclo@iastate.edu>
2025-04-07 13:44:17 -0500

    fix

--- ros_ws/src/graph_points/scripts/raw.py
+++ ros_ws/src/graph_points/scripts/raw.py
@@ -5,6 +5,11 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import time
 
+
+def on_close(event):
+    rospy.signal_shutdown("Matplotlib window closing")
+
+
 plt.ion() 
 fig = plt.figure()
 fig.canvas.mpl_connect('close_event', on_close)
@@ -14,38 +19,6 @@ ax.view_init(elev=90, azim=90, roll=180)  # Top-down view looking at the XY plan
 instants_received = [0, 0]
 list_of_x_coord, list_of_y_coord, list_of_z_coord = [], [], []
 
-
-def app():
-    sub = rospy.Subscriber('landmarks', Landmarks, get_plotting_callback())
-    rospy.init_node('POSE_Display_Raw_Landmarks', anonymous=True)
-    all_orange_colors = ['orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange']
-    while not rospy.is_shutdown():
-        ax.set_xlim([-640, 640])
-        ax.set_ylim([0, 480])
-        ax.set_zlim([4, 0])
-        ax.set_xlabel("X (Left/Right)")
-        ax.set_ylabel("Y (Up/Down)")
-        ax.set_zlabel("Z (Forward/Backward)")
-        ax.set_box_aspect([16, 6, 1])
-
-        plt.draw()
-
-        plt.pause(0.001)  
-
-        # If the last frame was more than a tenth of a second ago, skip redrawing it
-        if time.time() - instants_received[0] > .1 and time.time() - instants_received[1] > .1:
-            continue
-
-        ax.clear()
-        ax.scatter(list_of_x_coord, list_of_y_coord, list_of_z_coord, color=all_orange_colors)
-
-    print("exited loop")
-    
-if __name__ == '__main__':
-    try:
-        app()
-    except rospy.ROSInterruptException:
-        pass
 
 def get_plotting_callback(msg):
     global instants_received, list_of_z_coord_lists, list_of_x_coord_lists, list_of_y_coord_lists
@@ -106,6 +79,35 @@ def get_plotting_callback(msg):
 return plot_landmarks_in_3d
 
 
-def on_close(event):
-    rospy.signal_shutdown("Matplotlib window closing")
+def app():
+    sub = rospy.Subscriber('landmarks', Landmarks, get_plotting_callback())
+    rospy.init_node('POSE_Display_Raw_Landmarks', anonymous=True)
+    all_orange_colors = ['orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange']
+    while not rospy.is_shutdown():
+        ax.set_xlim([-640, 640])
+        ax.set_ylim([0, 480])
+        ax.set_zlim([4, 0])
+        ax.set_xlabel("X (Left/Right)")
+        ax.set_ylabel("Y (Up/Down)")
+        ax.set_zlabel("Z (Forward/Backward)")
+        ax.set_box_aspect([16, 6, 1])
+
+        plt.draw()
+
+        plt.pause(0.001)  
+
+        # If the last frame was more than a tenth of a second ago, skip redrawing it
+        if time.time() - instants_received[0] > .1 and time.time() - instants_received[1] > .1:
+            continue
+
+        ax.clear()
+        ax.scatter(list_of_x_coord, list_of_y_coord, list_of_z_coord, color=all_orange_colors)
+
+    print("exited loop")
+    
+if __name__ == '__main__':
+    try:
+        app()
+    except rospy.ROSInterruptException:
+        pass
 
--- ros_ws/src/graph_points/scripts/scaled.py
+++ ros_ws/src/graph_points/scripts/scaled.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python3
 import rospy
 from custom_msg.msg import Landmarks
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import time
+
+
+def on_close(event):
+    rospy.signal_shutdown("Matplotlib window closing")
+
 
 plt.ion()
 fig = plt.figure()
@@ -13,41 +19,6 @@ ax.view_init(elev=90, azim=90, roll=180)
 
 instants_received = [0, 0]
 list_of_x_coord, list_of_y_coord, list_of_z_coord = [], [], []
-
-
-def app():
-    sub = rospy.Subscriber('scaled', Landmarks, get_plotting_callback)
-    rospy.init_node('POSE_Display_Scaled', anonymous=True)
-    all_green_colors = ['green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green']
-    while not rospy.is_shutdown():
-        ax.set_xlim([-1, 1])
-        ax.set_ylim([0, 1.5])
-        ax.set_zlim([2, -2])
-        ax.set_xlabel("X (Left/Right)")
-        ax.set_ylabel("Y (Up/Down)")
-        ax.set_zlabel("Z (Forward/Backward)")
-        ax.set_box_aspect([5, 3.75, 4])
-
-        plt.draw()
-
-        plt.pause(0.001)  
-
-        # If the last frame was more than a tenth of a second ago, skip redrawing it
-        if time.time() - instants_received[0] > .1 and time.time() - instants_received[1] > .1:
-            continue
-
-        ax.clear()
-
-        # Transpose from robot coordinate system to the "normal" one
-        ax.scatter(list_of_z_coord, list_of_x_coord, list_of_y_coord, color=all_green_colors)
-
-    print("exited loop")
-    
-if __name__ == '__main__':
-    try:
-        app()
-    except rospy.ROSInterruptException:
-        pass
 
 
 def get_plotting_callback(msg):
@@ -106,9 +77,41 @@ def get_plotting_callback(msg):
         msg.right_thumb[2],
         msg.nose[2]
     ]
-return plot_landmarks_in_3d
+    return plot_landmarks_in_3d
 
 
-def on_close(event):
-    rospy.signal_shutdown("Matplotlib window closing")
+def app():
+    sub = rospy.Subscriber('scaled', Landmarks, get_plotting_callback)
+    rospy.init_node('POSE_Display_Scaled', anonymous=True)
+    all_green_colors = ['green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green']
+    while not rospy.is_shutdown():
+        ax.set_xlim([-1, 1])
+        ax.set_ylim([0, 1.5])
+        ax.set_zlim([2, -2])
+        ax.set_xlabel("X (Left/Right)")
+        ax.set_ylabel("Y (Up/Down)")
+        ax.set_zlabel("Z (Forward/Backward)")
+        ax.set_box_aspect([5, 3.75, 4])
+
+        plt.draw()
+
+        plt.pause(0.001)  
+
+        # If the last frame was more than a tenth of a second ago, skip redrawing it
+        if time.time() - instants_received[0] > .1 and time.time() - instants_received[1] > .1:
+            continue
+
+        ax.clear()
+
+        # Transpose from robot coordinate system to the "normal" one
+        ax.scatter(list_of_z_coord, list_of_x_coord, list_of_y_coord, color=all_green_colors)
+
+    print("exited loop")
+    
+if __name__ == '__main__':
+    try:
+        app()
+    except rospy.ROSInterruptException:
+        pass
+
 
