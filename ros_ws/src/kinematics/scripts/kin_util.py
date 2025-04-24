202aeeec9fbf963bea87760c4f645b6476be0124
Max Mayer-Mader <maxmayermader2@gmail.com>
2025-04-24 14:45:11 -0500

    added interporatlion to util

--- ros_ws/src/kinematics/scripts/kin_util.py
+++ ros_ws/src/kinematics/scripts/kin_util.py
@@ -1,5 +1,17 @@
 from time import time
 
+SPEED_THRESHOLD = 2.0  # rad/s
+
 def timestamp() -> int:
     """Returns the current time in milliseconds since the epoch using time()."""
     return int(time() * 1000)
+
+def interpolate_points(start_angles, end_angles, num_steps):
+    if num_steps <= 0:
+        return []
+    interpolated = []
+    for step in range(1, num_steps + 1):
+        alpha = step / num_steps  # Ranges from 1/num_steps to 1.0
+        interp = [s + alpha * (e - s) for s, e in zip(start_angles, end_angles)]
+        interpolated.append(interp)
+    return interpolated
